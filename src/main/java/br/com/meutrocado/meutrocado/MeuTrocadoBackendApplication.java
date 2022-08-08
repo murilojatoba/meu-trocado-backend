0edcc74646897a689b32dd898d78a40c27bb959f
murilo <murilo@pop-os.localdomain>
2022-08-08 11:13:48 -0300

    [001] Inclusão do Spring Boot.

---
+++ src/main/java/br/com/meutrocado/meutrocado/MeuTrocadoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.meutrocado.meutrocado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuTrocadoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuTrocadoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meutrocado/meutrocado/MeuTrocadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meutrocado.meutrocado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuTrocadoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
